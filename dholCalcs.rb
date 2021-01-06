74aa7a955d6649839248736d5b36b6cefa25f1c7
nigamelastic <nigamelastic@gmail.com>
2021-01-06 01:12:24 +0100

    adding the first two ruby files

---
+++ dholCalcs.rb
@@ -0,0 +1,14 @@
+dhol= "C:/Users/kshit/Desktop/multimedia/bhangra_dhol_mp3/bhangradhol1.wav"
+print sample_duration dhol
+live_loop :bhangradhol do
+  
+  
+  
+  sample dhol, finish: 0.32
+  sleep 1.25
+  
+  
+end
+
+
+
---
+++ nominal.rb
@@ -0,0 +1,16 @@
+
+# HIHAT
+define :hihat do
+  use_synth :pnoise
+  with_fx :hpf, cutoff: 120 do
+    play release: 0.01, amp: 13
+  end
+end
+
+live_loop :hihat_loop do
+  divisors = ring 2, 4, 2, 2, 2, 2, 2, 6
+  divisors.tick.times do
+    hihat
+    sleep 1.0 / divisors.look
+  end
+end
