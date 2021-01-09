bce7d03990af5f8768ea79d830913dda4eb1ff06
Kshitij Nigam <nigamelastic@gmail.com>
2021-01-10 00:32:07 +0100

    Create dhagenatinaktin.rb

---
+++ dhagenatinaktin.rb
@@ -0,0 +1,34 @@
+
+loop do
+  with_fx :reverb do
+    sample :tabla_na
+    sample :tabla_ghe2
+    sleep 0.3
+    
+    sample :tabla_ghe2
+    
+    sleep 0.3
+    sample :tabla_na
+    
+    sleep 0.3
+    sample :tabla_te1
+    sleep 0.3
+    sample :tabla_na
+    sleep 0.3
+    sample :tabla_ghe2
+    sleep 0.3
+    sample :tabla_na
+    sample :tabla_ghe2
+    
+    sleep 0.3
+    sample :tabla_na
+    
+    
+    
+    sleep 0.3
+    
+  end
+  
+end
+
+
